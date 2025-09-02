306b74a16f9ef88dfb2b345ecb774c0feeda4873
liuyankai <liuyankai23@mails.ucas.ac.cn>
2025-09-02 12:49:58 +0800

    评估分数31.649

--- src/evaluate.py
+++ src/evaluate.py
@@ -4,19 +4,19 @@ from tqdm import tqdm
 
 from dataset import get_dataloader
 import config
-from model import TrainslationEncoder, TrainslationDecoder
+from model import TranslationModel
 from tokenizer import ChineseTokenizer, EnglishTokenizer
 from predict import predict_batch
 
 
-def evaluate(dataloader, encoder, decoder, zh_tokenizer, en_tokenizer, device):
+def evaluate(dataloader, model, zh_tokenizer, en_tokenizer, device):
     references = [] # [batch_size, 1, seq_len]
     predictions = [] # [batch_size, seq_len]
     special_tokens = [en_tokenizer.eos_token_id,en_tokenizer.sos_token_id,en_tokenizer.pad_token_id]
     for inputs,targets in tqdm(dataloader,desc="评估"):
         inputs = inputs.to(device) # [batch_size, seq_len]
         targets = targets.tolist() # [batch_size, seq_len]
-        batch_result = predict_batch(inputs,encoder,decoder,zh_tokenizer,en_tokenizer,device)
+        batch_result = predict_batch(inputs,model,zh_tokenizer,en_tokenizer,device)
         predictions.extend(batch_result)
         # references.extend([[target] for target in targets])
         references.extend([[[index for index in target if index not in special_tokens]] for target in targets])
@@ -30,15 +30,14 @@ def run_evaluate():
     en_tokenizer = EnglishTokenizer.from_vocab(config.PROCESS_DATA_DIR / 'en_vocab_txt')
 
     # 准备模型
-    encoder = TrainslationEncoder(vocab_size=zh_tokenizer.vocab_size, padding_index=zh_tokenizer.pad_token_id).to(
-        device)
-    decoder = TrainslationDecoder(vocab_size=en_tokenizer.vocab_size, padding_index=en_tokenizer.pad_token_id).to(
-        device)
-    encoder.load_state_dict(torch.load(config.MODELS_DIR / 'encoder.pt'))
-    decoder.load_state_dict(torch.load(config.MODELS_DIR / 'decoder.pt'))
+    model = TranslationModel(zh_vocab_size=zh_tokenizer.vocab_size,
+                             en_vocab_size=en_tokenizer.vocab_size,
+                             zh_padding_idx=zh_tokenizer.pad_token_id,
+                             en_padding_idx=en_tokenizer.pad_token_id).to(device)
+    model.load_state_dict(torch.load(config.MODELS_DIR / 'model.pt'))
 
     dataloader = get_dataloader(train=False)
-    bleu = evaluate(dataloader,encoder,decoder,zh_tokenizer,en_tokenizer,device)
+    bleu = evaluate(dataloader,model,zh_tokenizer,en_tokenizer,device)
     print(f"bleu:{bleu}")
 
 
